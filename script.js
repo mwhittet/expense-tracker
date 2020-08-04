264970f1d8d25fd2dbad4b700dbca8e42883ff85
mwhittet <5453237+mwhittet@users.noreply.github.com>
2020-08-04 01:59:27 +0100

    Add transaction values and edit wording

--- script.js
+++ script.js
@@ -33,10 +33,32 @@ const addTransactionToDOM = (transaction) => {
   list.appendChild(item);
 };
 
+// Update the balence, income and expenses
+const updateValues = () => {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expenses = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `£${total}`;
+  money_plus.innerText = `£${income}`;
+  money_minus.innerText = `£${expenses}`;
+};
+
 // Init app
 const init = () => {
   list.innerHTML = '';
   transactions.forEach(addTransactionToDOM);
+  updateValues();
 };
 
 init();
